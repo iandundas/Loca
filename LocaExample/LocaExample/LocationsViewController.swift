628757ea06d3d5a5d8680d7969a1e06a0f4541c7
Ian Dundas <contact@iandundas.co.uk>
2016-11-24 19:18:04 +0700

    Basic layout with buttons

---
+++ LocaExample/LocaExample/LocationsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsViewController.swift
+//  LocaExample
+//
+//  Created by Ian Dundas on 24/11/2016.
+//  Copyright © 2016 Tacks. All rights reserved.
+//
+
+import UIKit
+
+class LocationsViewController: UIViewController{
+    
+    
+    @IBAction func didTapStart(sender: AnyObject) {
+        print("Tapped!")
+    }
+    
+}
---
+++ LocaExample/LocaExample/PermissionsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  LocaExample
+//
+//  Created by Ian Dundas on 24/11/2016.
+//  Copyright © 2016 Tacks. All rights reserved.
+//
+
+import UIKit
+import ReactiveKit
+import Loca
+
+class PermissionsViewController: UIViewController {
+
+    private let authProvider = LocationAuthorizationProvider()
+    
+    let stream = PushStream<Void>()
+    
+    @IBAction func tappedPermission(sender: AnyObject) {
+        authProvider.authorize().observe { [weak self] event in
+            guard let strongSelf = self else {return}
+            
+            switch(event){
+            case .Completed:
+                strongSelf.performSegueWithIdentifier("ShowLocation", sender: nil)
+            case .Failure(_):
+                print("failed")
+            default:break;
+            }
+        }.disposeIn(rBag)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
