d59904a11f18d33b4e0f7b00def53d490fb64094
Ian Dundas <contact@iandundas.co.uk>
2016-11-24 16:21:37 +0700

    Created Loca framework

---
+++ Loca/Loca.h
@@ -0,0 +1,19 @@
+//
+//  Loca.h
+//  Loca
+//
+//  Created by Ian Dundas on 24/11/2016.
+//  Copyright © 2016 Tacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Loca.
+FOUNDATION_EXPORT double LocaVersionNumber;
+
+//! Project version string for Loca.
+FOUNDATION_EXPORT const unsigned char LocaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loca/PublicHeader.h>
+
+
